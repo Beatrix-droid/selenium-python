c94dffaf08bd0f3b5f34ccf5b02f6127c2541d76
Beatrice <beatrice.federici123@gmail.com>
2023-03-14 12:53:09 +0000

    inital commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from config import credentials
+from datetime import datetime
 
+#initialise browser
+options = Options()
+options.binary_location = '/usr/bin/firefox'
+options.headless=True
+browser = webdriver.Firefox(options=options)
+
+
+#check if it is friday or the end of the month:
+
+today = date.today()
+print(today)
+# go to login portal
+browser.get('https://softwareinstitute.bamboohr.com/login.php')
+js-normalLoginLink
+# click 'login' with email and password:
+buttons=driver.find_element(By.TAG_NAME, 'button')
+button=buttons.find_element(By.ClASS_NAME, "js-normalLoginLink").click()
+
+
+# type in username and password:
+inputs=driver.find_element(By.TAG_NAME, "input")
+email= inputs.find_element().send_keys(credentials["username"])
