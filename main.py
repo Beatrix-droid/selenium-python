3ebd5bc509fa0f35a380ad7a2650497571f2c006
Beatrice <beatrice.federici123@gmail.com>
2023-03-17 10:48:40 +0000

    added some helium functions

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
 from config import credentials
+from helium import *
 from datetime import date
 from calendar import monthrange
 
+from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
 #initialise browser
-options = Options()
-options.binary_location = '/usr/bin/firefox'
-options.headless=True
-browser = webdriver.Firefox(options=options)
+
+
 
 
 #check if it is friday or the end of the month:
@@ -28,14 +26,16 @@ if current_day.day==monthrange(current_day.year, current_day.month)[1]:
 print(monthrange(current_day.year, current_day.month), current_day.day)
 """
 
-# go to login portal
-browser.get('https://softwareinstitute.bamboohr.com/login.php')
+#initialise browser
+options= FirefoxOptions()
+browser=start_firefox('https://softwareinstitute.bamboohr.com/login.php', options=options, headless=True)
 
-# click 'login' with email and password:
+# click 'login with email and password':
+click("Log in with Email and Password")
 
-classic_login=browser.find_element(By.CSS_SELECTOR,".fab-link").click()
 
 
+#type into login form and submit form:
 # identify login form:
 email= browser.find_element(By.CSS_SELECTOR, "#lemail")
 password=browser.find_element(By.CSS_SELECTOR, "#password")
@@ -43,15 +43,32 @@ password=browser.find_element(By.CSS_SELECTOR, "#password")
 password.send_keys(credentials["password"])
 email.send_keys(credentials["username"])
 
-#submit form 
-submit_button=browser.find_element(By.XPATH,"//button[@type='submit']")
-submit_button.click()
-#confirm tp user we have logged in
+click("Log In")
+
+# confirm to user we have logged in
+title=browser.find_element(By.TAG_NAME, "span")
+print(title.text)
 print("succesfully logged in!")
 
-#locate my "my timesheet" button and click on it
-timesheet_link=browser.find_element(By.LINK_TEXT, "My Timesheet").click()
-
+# locate my "my timesheet" button and click on it
+click("My Timesheet")
+# confirm that the timesheet was opened
+assert Text("Timesheet").exists()
 print("opened by time sheet")
 
+
+#begin filling in the work hours
+week=["Mon", "Tue", "Wed", "Thur", "Fri"]
+for day in week:
+    click(day)
+    click("Add Time Entry")
+    if Text("Day Total: 7h 30m").exists() or Text("Day Total: 8h").exists() :
+        click("Cancel")  # if there are alreaddy hrs logged in skip
     
+    else:   # log the hours and click save
+        write("7.5")
+        click("--Select Project/Task--")
+        click("Liberty Global Automation & AI")
+        click("Save")
+        # confirm that we are back to the timesheet page
+        assert Text("Timesheet").exists()
