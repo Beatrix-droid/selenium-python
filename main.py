23824b8db4e3b9c8d7a3adbde5a9e6810d6a8e06
Beatrice <beatrice.federici123@gmail.com>
2023-03-24 17:17:09 +0000

    retrieved form and add hours tag

--- main.py
+++ main.py
@@ -54,7 +54,9 @@ assert page_has_loaded == True, "Error page not loaded correctly"
 
 
 # click 'login with email and password':
-normal_login_button = browser.find_element(By.XPATH, "//button[contains(text(),'Log in with Email and Password')]").click()
+normal_login_button = browser.find_element(
+    By.XPATH, "//button[contains(text(),'Log in with Email and Password')]"
+).click()
 # identify login form and check that it has loaded correctly:
 email = browser.find_element(By.CSS_SELECTOR, "#lemail")
 password = browser.find_element(By.CSS_SELECTOR, "#password")
@@ -86,34 +88,46 @@ assert my_name, " homepage not loaded correctly"
 my_timesheet = browser.find_element(By.LINK_TEXT, "My Timesheet")
 my_timesheet.click()
 
+browser.session_id
 # check that we have navigated to the timesheet page:
 h3_tags = browser.find_elements(By.TAG_NAME, "h3")
 
 h3_text = [tag.text for tag in h3_tags]
-if "Timesheet" not in h3_text:git commit  .
+if "Timesheet" not in h3_text:
     browser.save_screenshot("timesheet_not_found.png")
 assert "Timesheet" in h3_text, "timesheet page not found"
 
 
-#click on the time entries
-time_entries=browser.find_elements(By.CLASS_NAME, "Timesheet_Slat__addEntryLink")
-link_text=[time_entry.text for time_entry in time_entries]
-print(link_text)
-browser.save_screenshot(("time.png"))
+# click on the time entries
+time_sheet_form = browser.find_element(By.TAG_NAME, "form")
+
+#this will be for link in links, but it ammounts to finding the "add time entry" link
+link=time_sheet_form.find_element(By.TAG_NAME, "a")
+link.click()
+browser.save_full_page_screenshot("enter time.png")
+
+#check all the divs, if hrous worked is 7 30m close and do not fill in the sheet
+divs= browser.find_elements(By.CLASS_NAME, "div")
+
+div_texts=[div.text for div in divs]
+print(div_texts)
+#hours=browser.find_element(By.XPATH, "//div[text()='Day Total: 7h 30m']")
+
+#print(bool(hours))
 
 # begin filling in the work hours
 week = ["Mon", "Tue", "Wed", "Thur", "Fri"]
-#for day in week:
-    #click the day you need to fill in:
-#browser.find_element(By.XPATH, f"//span[text()='{day}']")
- #   click("Add Time Entry")
-  #  if Text("Day Total: 7h 30m").exists() or Text("Day Total: 8h").exists():
-   #     click("Cancel")  # if there are alreaddy hrs logged in skip
-
-    #else:  # log the hours and click save
-      #  write("7.5")
-     #   click("--Select Project/Task--")
-       # click("Liberty Global Automation & AI")
-        #click("Save")
-        ## confirm that we are back to the timesheet page
-        #assert Text("Timesheet").exists()
+# for day in week:
+# click the day you need to fill in:
+# browser.find_element(By.XPATH, f"//span[text()='{day}']")
+#   click("Add Time Entry")
+#  if Text("Day Total: 7h 30m").exists() or Text("Day Total: 8h").exists():
+#     click("Cancel")  # if there are alreaddy hrs logged in skip
+#/html/body/div[3]/section/div[2]/div[2]/div/div[3]/div[1]/form/div[4]/div[2]/div/div[1]/a
+# else:  # log the hours and click save
+#  write("7.5")
+#   click("--Select Project/Task--")
+# click("Liberty Global Automation & AI")
+# click("Save")
+# confirm that we are back to the timesheet page
+# assert Text("Timesheet").exists()
