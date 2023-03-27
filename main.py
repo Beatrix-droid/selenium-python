70b97fddb1dbadd8388f7d7be2bb3f15e61b4306
Beatrice <beatrice.federici123@gmail.com>
2023-03-27 13:33:56 +0000

    added time entries

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from config import credentials
-
+from time import sleep
 from datetime import date
 from calendar import monthrange
 from selenium import webdriver
@@ -47,30 +47,22 @@ spans = browser.find_elements(By.TAG_NAME, "span")
 text_spans = [span.text for span in spans]
 
 page_has_loaded = bool("Log in with SAML" in text_spans)
-
-if page_has_loaded == False:
-    browser.save_screenshot("login_page.png")
-assert page_has_loaded == True, "Error page not loaded correctly"
+assert page_has_loaded == True, "Error initial login page not loaded correctly" and  browser.save_screenshot("login_page.png")
 
 
 # click 'login with email and password':
-normal_login_button = browser.find_element(
-    By.XPATH, "//button[contains(text(),'Log in with Email and Password')]"
-).click()
+normal_login_button = browser.find_element(By.XPATH, "//button[contains(text(),'Log in with Email and Password')]").click()
 # identify login form and check that it has loaded correctly:
 email = browser.find_element(By.CSS_SELECTOR, "#lemail")
 password = browser.find_element(By.CSS_SELECTOR, "#password")
 submit_button = browser.find_element(By.XPATH, "//span[text()='Log In']")
 
-if submit_button == False or email == False or password == False:
-    browser.save_screenshot("no_submit_btn.png")
-assert submit_button and password and email, "Error page not loaded correctly"
+assert submit_button and password and email, "Error login form not loaded correctly" and browser.save_screenshot("no_submit_btn.png")
 
 
 # type into form
 email.send_keys(credentials["username"])
 password.send_keys(credentials["password"])
-
 
 # Submit form
 submit_button.click()
@@ -79,44 +71,62 @@ submit_button.click()
 
 # confirm to user we have logged in
 my_name = browser.find_element(By.XPATH, "//span[text()='Beatrice Federici']")
-
-if my_name == False:
-    browser.save_screenshot("home_page_not_found.png")
-assert my_name, " homepage not loaded correctly"
+assert my_name, " homepage not loaded correctly" and  browser.save_screenshot("home_page_not_found.png")
 
 # locate my "my timesheet" button and click on it
 my_timesheet = browser.find_element(By.LINK_TEXT, "My Timesheet")
 my_timesheet.click()
 
-browser.session_id
+
 # check that we have navigated to the timesheet page:
 h3_tags = browser.find_elements(By.TAG_NAME, "h3")
 
 h3_text = [tag.text for tag in h3_tags]
-if "Timesheet" not in h3_text:
-    browser.save_screenshot("timesheet_not_found.png")
-assert "Timesheet" in h3_text, "timesheet page not found"
+assert "Timesheet" in h3_text, "timesheet page not found" and browser.save_screenshot("timesheet_not_found.png")
 
 
 # click on the time entries
 time_sheet_form = browser.find_element(By.TAG_NAME, "form")
 
-#this will be for link in links, but it ammounts to finding the "add time entry" link
+# this will be for link in links, but it ammounts to finding the "add time entry" link
 link=time_sheet_form.find_element(By.TAG_NAME, "a")
 link.click()
-browser.save_full_page_screenshot("enter time.png")
 
-#check all the divs, if hrous worked is 7 30m close and do not fill in the sheet
-divs= browser.find_elements(By.CLASS_NAME, "div")
 
-div_texts=[div.text for div in divs]
-print(div_texts)
+# check how many hours you have worked for that particular day
+day_total=browser.find_element(By.CLASS_NAME, "AddEditEntry__dayTotal")
+
+assert day_total,"form for filling in hours not loaded correctly" and  browser.save_full_page_screenshot("hours+worked_form_not_found")
+
+hours_worked= day_total.text
+
+
+# locate input box and drop down menu, as well as the buttons
+input_box=browser.find_element(By.ID, "hoursWorked")
+drop_down=browser.find_element(By.XPATH, "//div[text()='--Select Project/Task--']")
+save_button=browser.find_element(By.XPATH, "//span[text()='Save']")
+cancel_button=browser.find_element(By.XPATH, "//span[text()='Cancel']")
+
+
+# fill in time sheet if hours worked appear 0:
+if hours_worked == "Day Total: 0h 00m":
+    input_box.send_keys("7.5")
+    drop_down.click()
+    select_automation=browser.find_element(By.CSS_SELECTOR, ".fab-MenuOption__row").click()
+    save_button.click()
+    sleep(3)
+    browser.save_full_page_screenshot("enter time.png")
+else:
+    cancel_button.click()
+    browser.save_full_page_screenshot("enter time.png")
+
+browser.quit()
+
 #hours=browser.find_element(By.XPATH, "//div[text()='Day Total: 7h 30m']")
 
 #print(bool(hours))
 
-# begin filling in the work hours
-week = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+
 # for day in week:
 # click the day you need to fill in:
 # browser.find_element(By.XPATH, f"//span[text()='{day}']")
