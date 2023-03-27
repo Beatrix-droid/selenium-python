8e77b6857eb8936b57620be3259a5efade820742
Beatrice <beatrice.federici123@gmail.com>
2023-03-27 14:51:31 +0000

    did logic that filled timesheet

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from config import credentials
-from time import sleep
 from datetime import date
 from calendar import monthrange
 from selenium import webdriver
-
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
-# initialise browser
+from time import sleep
+from config import credentials
 
+# initialise browser
+# wait.until(ExpectedConditions.elementToBeClickable(By.id("Login")));
 
 # check if it is friday or the end of the month:
 """
@@ -37,7 +37,6 @@ options.add_argument("--headless")
 browser = webdriver.Firefox(options=options)
 
 
-
 # navigate to the login page
 browser.get("https://softwareinstitute.bamboohr.com/login.php")
 
@@ -47,17 +46,23 @@ spans = browser.find_elements(By.TAG_NAME, "span")
 text_spans = [span.text for span in spans]
 
 page_has_loaded = bool("Log in with SAML" in text_spans)
-assert page_has_loaded == True, "Error initial login page not loaded correctly" and  browser.save_screenshot("login_page.png")
+assert page_has_loaded, "Error initial login page not loaded correctly" and browser.save_screenshot("login_page.png")
 
 
 # click 'login with email and password':
-normal_login_button = browser.find_element(By.XPATH, "//button[contains(text(),'Log in with Email and Password')]").click()
+normal_login_button = browser.find_element(
+    By.XPATH, "//button[contains(text(),'Log in with Email and Password')]"
+).click()
 # identify login form and check that it has loaded correctly:
 email = browser.find_element(By.CSS_SELECTOR, "#lemail")
 password = browser.find_element(By.CSS_SELECTOR, "#password")
 submit_button = browser.find_element(By.XPATH, "//span[text()='Log In']")
 
-assert submit_button and password and email, "Error login form not loaded correctly" and browser.save_screenshot("no_submit_btn.png")
+assert (
+    submit_button and password and email
+), "Error login form not loaded correctly" and browser.save_screenshot(
+    "no_submit_btn.png"
+)
 
 
 # type into form
@@ -71,7 +76,9 @@ submit_button.click()
 
 # confirm to user we have logged in
 my_name = browser.find_element(By.XPATH, "//span[text()='Beatrice Federici']")
-assert my_name, " homepage not loaded correctly" and  browser.save_screenshot("home_page_not_found.png")
+assert my_name, " homepage not loaded correctly" and browser.save_screenshot(
+    "home_page_not_found.png"
+)
 
 # locate my "my timesheet" button and click on it
 my_timesheet = browser.find_element(By.LINK_TEXT, "My Timesheet")
@@ -82,49 +89,58 @@ my_timesheet.click()
 h3_tags = browser.find_elements(By.TAG_NAME, "h3")
 
 h3_text = [tag.text for tag in h3_tags]
-assert "Timesheet" in h3_text, "timesheet page not found" and browser.save_screenshot("timesheet_not_found.png")
+assert "Timesheet" in h3_text, "timesheet page not found" and browser.save_screenshot(
+    "timesheet_not_found.png"
+)
 
 
 # click on the time entries
 time_sheet_form = browser.find_element(By.TAG_NAME, "form")
-
+browser.maximize_window()
 # this will be for link in links, but it ammounts to finding the "add time entry" link
-link=time_sheet_form.find_element(By.TAG_NAME, "a")
-link.click()
+links = time_sheet_form.find_elements(By.TAG_NAME, "a")
 
+for link in links:
+    sleep(2)
+    link.click()
+    
+    # check how many hours you have worked for that particular day
+    day_total = browser.find_element(By.CLASS_NAME, "AddEditEntry__dayTotal")
 
-# check how many hours you have worked for that particular day
-day_total=browser.find_element(By.CLASS_NAME, "AddEditEntry__dayTotal")
+    assert day_total, (
+        "form for filling in hours not loaded correctly"
+        and browser.save_full_page_screenshot("hours+worked_form_not_found.pmg")
+    )
 
-assert day_total,"form for filling in hours not loaded correctly" and  browser.save_full_page_screenshot("hours+worked_form_not_found")
+    hours_worked = day_total.text
 
-hours_worked= day_total.text
+    # locate input box and drop down menu, as well as the buttons
+    input_box = browser.find_element(By.ID, "hoursWorked")
+    drop_down = browser.find_element(
+        By.XPATH, "//div[text()='--Select Project/Task--']"
+    )
+    save_button = browser.find_element(By.XPATH, "//span[text()='Save']")
+    cancel_button = browser.find_element(By.XPATH, "//span[text()='Cancel']")
 
-
-# locate input box and drop down menu, as well as the buttons
-input_box=browser.find_element(By.ID, "hoursWorked")
-drop_down=browser.find_element(By.XPATH, "//div[text()='--Select Project/Task--']")
-save_button=browser.find_element(By.XPATH, "//span[text()='Save']")
-cancel_button=browser.find_element(By.XPATH, "//span[text()='Cancel']")
-
-
-# fill in time sheet if hours worked appear 0:
-if hours_worked == "Day Total: 0h 00m":
-    input_box.send_keys("7.5")
-    drop_down.click()
-    select_automation=browser.find_element(By.CSS_SELECTOR, ".fab-MenuOption__row").click()
-    save_button.click()
-    sleep(3)
-    browser.save_full_page_screenshot("enter time.png")
-else:
-    cancel_button.click()
-    browser.save_full_page_screenshot("enter time.png")
-
+    # fill in time sheet if hours worked appear 0:
+    if hours_worked == "Day Total: 0h 00m":
+        input_box.send_keys("7.5")
+        drop_down.click()
+        select_automation = browser.find_element(
+            By.CSS_SELECTOR, ".fab-MenuOption__row"
+        ).click()
+        save_button.click()
+        if links.index(link) == (len(links) - 1):
+            print("time sheet filled")
+    else:
+        cancel_button.click()
+        continue
+browser.save_full_page_screenshot("timesheet.png")
 browser.quit()
 
-#hours=browser.find_element(By.XPATH, "//div[text()='Day Total: 7h 30m']")
+# hours=browser.find_element(By.XPATH, "//div[text()='Day Total: 7h 30m']")
 
-#print(bool(hours))
+# print(bool(hours))
 
 
 # for day in week:
@@ -133,7 +149,7 @@ browser.quit()
 #   click("Add Time Entry")
 #  if Text("Day Total: 7h 30m").exists() or Text("Day Total: 8h").exists():
 #     click("Cancel")  # if there are alreaddy hrs logged in skip
-#/html/body/div[3]/section/div[2]/div[2]/div/div[3]/div[1]/form/div[4]/div[2]/div/div[1]/a
+# /html/body/div[3]/section/div[2]/div[2]/div/div[3]/div[1]/form/div[4]/div[2]/div/div[1]/a
 # else:  # log the hours and click save
 #  write("7.5")
 #   click("--Select Project/Task--")
