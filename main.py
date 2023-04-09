74e3a804642ed351d955b7eaa6dd4e1df73c366b
Beatrice <beatrice.federici123@gmail.com>
2023-04-09 11:08:39 +0000

    installed firefox 100.0.2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from calendar import monthrange
 from datetime import date
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
@@ -11,6 +10,11 @@ from selenium.webdriver.support.relative_locator import locate_with
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
+import logging
+
+
+
+logging.basicConfig(level=logging.DEBUG)
 
 USER_NAME = os.environ.get("USERNAME")
 USER_PASSWORD = os.environ.get("PASSWORD")
